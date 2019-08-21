d91c428457601b7d2780e1ae2d51ea543fe18b15
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-08-21 20:11:10 +0200

    Some improvements, add valueOf function (1.1)

--- src/Enum.class.php
+++ src/Enum.class.php
@@ -4,29 +4,42 @@ abstract class Enum
 {
 	/**
 	 * Returns an array with all keys and their values of this enum.
+	 *
 	 * @return array
 	 */
-	public static function all() : array
+	static function all(): array
 	{
 		return (new \ReflectionClass(get_called_class()))->getConstants();
 	}
 
 	/**
+	 * Returns the value of the constant with the given name or null if it doesn't exist.
+	 *
+	 * @param string $name The name of the constant. Case sensitive!
+	 */
+	static function valueOf(string $name)
+	{
+		return @static::all()[$name];
+	}
+
+	/**
 	 * Returns true if this enum has a constant with the given name.
-	 * @param string $name
+	 *
+	 * @param string $name The name of the constant. Case sensitive!
 	 * @return boolean
 	 */
-	public static function validateName(string $name) : bool
+	static function validateName(string $name): bool
 	{
 		return array_key_exists($name, static::all());
 	}
 
 	/**
 	 * Returns true if this enum has a constant with the given value.
+	 *
 	 * @param mixed $value
 	 * @return boolean
 	 */
-	public static function validateValue($value) : bool
+	static function validateValue($value): bool
 	{
 		return in_array($value, static::all());
 	}
